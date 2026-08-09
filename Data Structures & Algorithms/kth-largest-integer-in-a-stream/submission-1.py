718e58a44fe05a107b601f86b0fe523c06dbee68
Tass-1 <priyanshujoshi10000@gmail.com>
2026-08-09 18:25:50 +0530

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.py
@@ -0,0 +1,18 @@
+import heapq
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.himin = nums
+        heapq.heapify(self.himin)
+
+        while len(self.himin) > self.k:
+            heapq.heappop(self.himin)
+
+        
+    def add(self, val: int) -> int:
+        heapq.heappush(self.himin , val)
+        if len(self.himin) > self.k:
+            heapq.heappop(self.himin)
+        print(self.himin)
+        return self.himin[0]
